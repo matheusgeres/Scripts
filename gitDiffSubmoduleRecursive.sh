190fe24c686303baf29da902827e9a809cfef611
Matheus Ferreira Geres <matheus.geres@keyrus.com.br>
2019-09-17 12:43:53 -0300

    Correção do ponto onde era removido o arquivo de change e remoção do STDOUT do git fetch

--- gitDiffSubmoduleRecursive.sh
+++ gitDiffSubmoduleRecursive.sh
@@ -8,17 +8,20 @@ FILE_CHANGE_GIT="/tmp/change-git"
 function checkDiff {
   GIT_DIR="--git-dir="$1"/.git"
   CURRENT_BRANCH=`git $GIT_DIR branch | grep \* | cut -d ' ' -f2`
-  git $GIT_DIR fetch -a
+  git $GIT_DIR fetch -a 1>/dev/null
   git $GIT_DIR diff $CURRENT_BRANCH origin/$2 | tee -a $FILE_CHANGE_GIT 1>/dev/null
 
   # Verificando se há diff com base no arquivo gerado
   if [ -s $FILE_CHANGE_GIT ]
   then
-    echo "There is a diff! Push your branch, dude! :D "
-    echo "git $GIT_DIR push --set-upstream origin $CURRENT_BRANCH"
+    echo "    ==> There is a diff! Push your branch, dude! :D"
+    echo "    git $GIT_DIR push --set-upstream origin $CURRENT_BRANCH"
   else
-    echo "There is no diff! Enjoy your day! ;D"
+    echo "    There is no diff! Enjoy your day! ;D"
   fi
+
+  # Limpando o arquivo de change
+  rm $FILE_CHANGE_GIT
 }
 
 # Obtém os submódulos
@@ -33,7 +36,3 @@ for sub in "${listSub[@]}" ; do
   # echo `git --git-dir=$sub/.git branch`
   checkDiff $sub $BRANCH_DEST
 done
-
-# Limpando o arquivo de change
-rm $FILE_CHANGE_GIT
-
